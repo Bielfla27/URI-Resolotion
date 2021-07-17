8b895728781ade277af40a262735c2010d6a887b
Bielfla27 <gabibielmft@outlook.com>
2021-07-16 22:29:30 -0300

    teste

---
+++ Java/1001.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x,b;
+		x =sc.nextInt();
+		b =sc.nextInt();
+		System.out.printf("X = %d\n" , x+b);
+		sc.close();
+	}
+
+}
